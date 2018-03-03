49547437aa1572d344265985ac8a2385cd75fc88
hjzh <hjzh578@gmail.com>
2018-03-04 04:23:03 +0800

    merge

--- piano.py
+++ piano.py
@@ -2,15 +2,43 @@ import Leap, sys
 import pygame
 import winsound
 import time
+import multiprocessing
+
+BLACK_HEIGHT = 200
+BLACK_WIDTH = 10
+WHITE_HEIGHT = 500
+WHITE_WIDTH = 50
 
 # creating the event
 KEYTAP = pygame.USEREVENT + 1
 KEYDRAWFINGERS = pygame.USEREVENT + 2
 
-FREQUENCY = [261, 293, 330, 349, 392, 440, 494, 523]
+FREQUENCY = [261, 277, 293, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 699, 740, 784, 830, 880, 932, 988]
+
+class Key:
+    frequency = 0
+    duration = 0
+    color = (0, 0, 0)
+    kind = "white"
+    def __init__(self, frequency, duration, kind):
+        self.frequency = frequency
+        self.duration = duration
+        self.kind = kind
+        if (self.kind == "white"):
+            color = (255, 255, 255)
+        else:
+            color = (0, 0, 0)
+
+    def beep(self):
+        winsound.Beep(self.frequency, self.duration)
+
+    def changeColor(self):
+        self.color = (0, 255, 0)
 
 class SampleListener(Leap.Listener):
     tap_num = 0
+    fingerNums = ['thumb', 'index', 'middle', 'ring', 'pinky'];
+    handNums = ['left', 'right'];
     minx = -100
     maxx = 100
     minz = -50
@@ -25,41 +53,35 @@ class SampleListener(Leap.Listener):
 
     def on_frame(self, controller):
         frame = controller.frame()
-        iBox = frame.interaction_box
-
         # draw finger projection
-        fingers = []
-        for finger in frame.fingers:
-            leapPoint = finger.stabilized_tip_position
-            normalizedPoint = iBox.normalize_point(leapPoint, False)
-            # print(finger.tip_position)
-            finger_pos = [normalizedPoint.x * 960, (1 - normalizedPoint.y) * 640]
-            # if (normalizedPoint[0] > self.minx and normalizedPoint[0] < self.maxx
-            #         and normalizedPoint[2] > self.minz and normalizedPoint[2] < self.maxz):
-            #     self.map2screen(finger.tip_position[0], finger.tip_position[2], finger_pos)
-            if (len(finger_pos) > 0):
-                fingers.append(finger_pos)
+        fingers = [];
+        done = False;
+        for i, hand in enumerate(frame.hands):
+            for j, finger in enumerate(hand.fingers):
+                finger_pos = []
+                if (finger.tip_position.x > self.minx and finger.tip_position.x < self.maxx
+                        and finger.tip_position.z > self.minz and finger.tip_position.z < self.maxz):
+                    self.map2screen(finger.tip_position.x, finger.tip_position.z, finger_pos)
+
+                    if (finger.tip_velocity.z < -750):
+                        temp = [finger.tip_position.x, finger.tip_position.z];
+                    else:
+                        temp = [];
+
+                    if (temp and done == False):  # true if finger is tapping
+                        print("Tap recorded " + self.handNums[i] + " " + self.fingerNums[j])
+                        self.tap_num = self.tap_num + 1
+                        # tappingFingers.append(temp);
+                        self.map2screen(temp[0], temp[1], finger_pos)
+                        key_tap_event = pygame.event.Event(KEYTAP, fingerpos=finger_pos)
+                        pygame.event.post(key_tap_event)
+                        done = True;
+
+                fingers.append(finger_pos);
 
         if len(fingers) > 0:
-            draw_finger_event = pygame.event.Event(KEYDRAWFINGERS, fingerspos = fingers)
+            draw_finger_event = pygame.event.Event(KEYDRAWFINGERS, fingerspos=fingers)
             pygame.event.post(draw_finger_event)
-
-        for gesture in frame.gestures():
-            if gesture.type is Leap.Gesture.TYPE_KEY_TAP:
-                key_tap = Leap.KeyTapGesture(gesture)
-                # print("one key tap recorded! %i" % self.tap_num)
-                self.tap_num = self.tap_num + 1
-                normalizedPoint = iBox.normalize_point(key_tap.position, False)
-                finger_pos = [normalizedPoint.x * 960, (1 - normalizedPoint.y) * 640]
-                # if (key_tap.position[0] > self.minx and key_tap.position[0] < self.maxx
-                #         and key_tap.position[2] > self.minz and key_tap.position[2] < self.maxz):
-                #     self.map2screen(key_tap.position[0], key_tap.position[2], finger_pos)
-                if (len(finger_pos) > 0):
-                    key_tap_event = pygame.event.Event(KEYTAP, fingerpos = finger_pos)
-                    pygame.event.post(key_tap_event)
-                # print(key_tap.position)
-
-        # print(frame.pointables.frontmost.tip_position)
 
 def main():
     pygame.init()
@@ -92,12 +114,19 @@ def main():
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
+    keys = []
+    for freq in FREQUENCY:
+        if ()
+        key = Key(freq, 300, "white")
+        keys.append(key)
+
     # # Keep this process running until Enter is pressed
     # print "Press Enter to quit..."
     # try:
     #     sys.stdin.readline()
     # except KeyboardInterrupt:
     #     pass
+    jobs = []
     while 1:
         # time.sleep(0.02)
         screen.blit(keyboard, (0, 0))
@@ -127,7 +156,10 @@ def main():
                 print("Index: ", index)
                 print(event.fingerpos)
                 if (index > -1):
-                    winsound.Beep(FREQUENCY[index], 100)
+                    p = multiprocessing.Process(target=keys[index].beep())
+                    jobs.append(p)
+                    p.start()
+
         pygame.display.flip()
 
 if __name__ == "__main__":
