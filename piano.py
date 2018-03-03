fe75e1d1929c772bf80c646f63a2a11d50df69ac
hjzh <hjzh578@gmail.com>
2018-03-03 23:45:52 +0800

    Add fingers drawing

--- piano.py
+++ piano.py
@@ -10,18 +10,33 @@ FREQUENCY = [261, 293, 330, 349, 392, 440, 494, 523]
 
 class SampleListener(Leap.Listener):
     tap_num = 0
+    minx = -150
+    maxx = 150
+    minz = -25
+    maxz = 75
 
     def on_connect(self, controller):
         print "Connected"
+
+    def map2screen(self, inx, inz, posout):
+        posout.append((inx - self.minx) / 300 * 960)
+        posout.append((inz - self.minz) / 100 * 360)
 
     def on_frame(self, controller):
         frame = controller.frame()
 
         # draw finger projection
+        fingers = []
         for finger in frame.fingers:
-            #
             # print(finger.tip_position)
-            draw_finger_event = pygame.event.Event(KEYDRAWFINGERS)
+            finger_pos = []
+            if (finger.tip_position[0] > self.minx and finger.tip_position[0] < self.maxx
+                    and finger.tip_position[2] > self.minz and finger.tip_position[2] < self.maxz):
+                self.map2screen(finger.tip_position[0], finger.tip_position[2], finger_pos)
+            fingers.append(finger_pos)
+
+        if len(fingers) > 0:
+            draw_finger_event = pygame.event.Event(KEYDRAWFINGERS, fingerspos = fingers)
             pygame.event.post(draw_finger_event)
 
         for gesture in frame.gestures():
@@ -29,7 +44,9 @@ class SampleListener(Leap.Listener):
                 key_tap = Leap.KeyTapGesture(gesture)
                 print("one key tap recorded! %i" % self.tap_num)
                 self.tap_num = self.tap_num + 1
-                key_tap_event = pygame.event.Event(KEYTAP)
+                finger_pos = []
+                self.map2screen(key_tap.position[0], key_tap.position[2], finger_pos)
+                key_tap_event = pygame.event.Event(KEYTAP, fingerpos = finger_pos)
                 pygame.event.post(key_tap_event)
 
                 # print(key_tap.position)
@@ -72,11 +89,22 @@ def main():
     #     sys.stdin.readline()
     # except KeyboardInterrupt:
     #     pass
-
     while 1:
+        screen.blit(keyboard, (0, 0))
+        # pygame.draw.circle(screen, (0, 125, 0), (450, 100), 20, 3)
+
+        has_drawn_fingers = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            if event.type == KEYDRAWFINGERS and not has_drawn_fingers:
+                for finger_pos in event.fingerspos:
+                    if (len(finger_pos) > 0):
+                        pygame.draw.circle(screen, (0, 0, 0), (int(finger_pos[0]), int(finger_pos[1])), 15, 3)
+                        print("one finger: ", finger_pos)
+                has_drawn_fingers = True
+                pygame.event.clear(KEYDRAWFINGERS)
 
             if event.type == KEYTAP:
                 print("Beep!")
